c17664bb81ceb62f0d0efa77288bab0d9237ef38
chiggy69 <chiragaggarwal69@gmail.com>
2023-05-28 03:09:32 +0530

    final mp

--- frontend/src/screens/Signup.js
+++ frontend/src/screens/Signup.js
@@ -12,21 +12,21 @@ import Container from "@mui/material/Container";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 function Copyright(props) {
-  return (
-    <Typography
-      variant="body2"
-      color="text.secondary"
-      align="center"
-      {...props}
-    >
-      {"Copyright © "}
-      <Link color="inherit" href="https://mui.com/">
-        Harsh Prakash
-      </Link>{" "}
-      {new Date().getFullYear()}
-      {"."}
-    </Typography>
-  );
+	return (
+		<Typography
+			variant="body2"
+			color="text.secondary"
+			align="center"
+			{...props}
+		>
+			{"Copyright © "}
+			<Link color="inherit" href="https://mui.com/">
+				Harsh Prakash
+			</Link>{" "}
+			{new Date().getFullYear()}
+			{"."}
+		</Typography>
+	);
 }
 
 // TODO remove, this demo shouldn't need to reset the theme.
@@ -34,154 +34,154 @@ function Copyright(props) {
 const defaultTheme = createTheme();
 
 export default function Signup() {
-  const navigate = useNavigate();
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    console.log({
-      name: data.get("name"),
-      email: data.get("email"),
-      password: data.get("password"),
-      username: data.get("username"),
-    });
+	const navigate = useNavigate();
+	const handleSubmit = async (event) => {
+		event.preventDefault();
+		const data = new FormData(event.currentTarget);
+		console.log({
+			name: data.get("name"),
+			email: data.get("email"),
+			password: data.get("password"),
+			username: data.get("username"),
+		});
 
-    const response = await fetch("http://localhost:5000/auth/register", {
-      method: "POST",
-      headers: {
-        "content-type": "application/json",
-      },
-      body: JSON.stringify({
-        email: data.get("email"),
-        password: data.get("password"),
-        name: data.get("name"),
-        username: data.get("username"),
-      }),
-    });
+		const response = await fetch("/api/auth/register", {
+			method: "POST",
+			headers: {
+				"content-type": "application/json",
+			},
+			body: JSON.stringify({
+				email: data.get("email"),
+				password: data.get("password"),
+				name: data.get("name"),
+				username: data.get("username"),
+			}),
+		});
 
-    const json = await response.json();
-    console.log(json);
+		const json = await response.json();
+		console.log(json);
 
-    // if (json.success) {
-    //   localStorage.setItem("jwttoken", json.token);
-    //   localStorage.setItem("user", json.user.name);
-    //   localStorage.setItem("username", json.user.username);
-    //   navigate(`/${localStorage.username}`);
-    // } else {
-    //   alert("Enter valid info");
-    // }
-    if (json.success) {
-      const loginResponse = await fetch("http://localhost:5000/auth/login", {
-        method: "POST",
-        headers: {
-          "content-type": "application/json",
-        },
-        body: JSON.stringify({
-          email: data.get("email"),
-          password: data.get("password"),
-        }),
-      });
+		// if (json.success) {
+		//   localStorage.setItem("jwttoken", json.token);
+		//   localStorage.setItem("user", json.user.name);
+		//   localStorage.setItem("username", json.user.username);
+		//   navigate(`/${localStorage.username}`);
+		// } else {
+		//   alert("Enter valid info");
+		// }
+		if (json.success) {
+			const loginResponse = await fetch("/api/auth/login", {
+				method: "POST",
+				headers: {
+					"content-type": "application/json",
+				},
+				body: JSON.stringify({
+					email: data.get("email"),
+					password: data.get("password"),
+				}),
+			});
 
-      const jsonLogin = await loginResponse.json();
-      console.log(jsonLogin);
+			const jsonLogin = await loginResponse.json();
+			console.log(jsonLogin);
 
-      if (json.success) {
-        localStorage.setItem("jwttoken", jsonLogin.token);
-        localStorage.setItem("user", jsonLogin.user.name);
-        localStorage.setItem("username", jsonLogin.user.username);
-        navigate(`/${localStorage.username}`);
-      } else {
-        alert("Invalid Credentials");
-      }
-    } else {
-      alert("Enter valid info");
-    }
-  };
+			if (json.success) {
+				localStorage.setItem("jwttoken", jsonLogin.token);
+				localStorage.setItem("user", jsonLogin.user.name);
+				localStorage.setItem("username", jsonLogin.user.username);
+				navigate(`/${localStorage.username}`);
+			} else {
+				alert("Invalid Credentials");
+			}
+		} else {
+			alert("Enter valid info");
+		}
+	};
 
-  return (
-    <ThemeProvider theme={defaultTheme}>
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <Box
-          sx={{
-            marginTop: 8,
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-          }}
-        >
-          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}></Avatar>
-          <Typography component="h1" variant="h5">
-            Sign up
-          </Typography>
-          <Box
-            component="form"
-            noValidate
-            onSubmit={handleSubmit}
-            sx={{ mt: 3 }}
-          >
-            <Grid container spacing={2}>
-              <Grid item xs={12} sm={12}>
-                <TextField
-                  autoComplete="given-name"
-                  name="name"
-                  required
-                  fullWidth
-                  id="name"
-                  label="Name"
-                  autoFocus
-                />
-              </Grid>
-              <Grid item xs={12} sm={12}>
-                <TextField
-                  name="username"
-                  required
-                  fullWidth
-                  id="username"
-                  label="Username"
-                  autoFocus
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  id="email"
-                  label="Email Address"
-                  name="email"
-                  autoComplete="email"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  name="password"
-                  label="Password"
-                  type="password"
-                  id="password"
-                  autoComplete="new-password"
-                />
-              </Grid>
-            </Grid>
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              Sign Up
-            </Button>
-            <Grid container justifyContent="flex-end">
-              <Grid item>
-                <Link to="/login" variant="body2">
-                  Already have an account? Sign in
-                </Link>
-              </Grid>
-            </Grid>
-          </Box>
-        </Box>
-        <Copyright sx={{ mt: 5 }} />
-      </Container>
-    </ThemeProvider>
-  );
+	return (
+		<ThemeProvider theme={defaultTheme}>
+			<Container component="main" maxWidth="xs">
+				<CssBaseline />
+				<Box
+					sx={{
+						marginTop: 8,
+						display: "flex",
+						flexDirection: "column",
+						alignItems: "center",
+					}}
+				>
+					<Avatar sx={{ m: 1, bgcolor: "secondary.main" }}></Avatar>
+					<Typography component="h1" variant="h5">
+						Sign up
+					</Typography>
+					<Box
+						component="form"
+						noValidate
+						onSubmit={handleSubmit}
+						sx={{ mt: 3 }}
+					>
+						<Grid container spacing={2}>
+							<Grid item xs={12} sm={12}>
+								<TextField
+									autoComplete="given-name"
+									name="name"
+									required
+									fullWidth
+									id="name"
+									label="Name"
+									autoFocus
+								/>
+							</Grid>
+							<Grid item xs={12} sm={12}>
+								<TextField
+									name="username"
+									required
+									fullWidth
+									id="username"
+									label="Username"
+									autoFocus
+								/>
+							</Grid>
+							<Grid item xs={12}>
+								<TextField
+									required
+									fullWidth
+									id="email"
+									label="Email Address"
+									name="email"
+									autoComplete="email"
+								/>
+							</Grid>
+							<Grid item xs={12}>
+								<TextField
+									required
+									fullWidth
+									name="password"
+									label="Password"
+									type="password"
+									id="password"
+									autoComplete="new-password"
+								/>
+							</Grid>
+						</Grid>
+						<Button
+							type="submit"
+							fullWidth
+							variant="contained"
+							sx={{ mt: 3, mb: 2 }}
+						>
+							Sign Up
+						</Button>
+						<Grid container justifyContent="flex-end">
+							<Grid item>
+								<Link to="/login" variant="body2">
+									Already have an account? Sign in
+								</Link>
+							</Grid>
+						</Grid>
+					</Box>
+				</Box>
+				<Copyright sx={{ mt: 5 }} />
+			</Container>
+		</ThemeProvider>
+	);
 }
